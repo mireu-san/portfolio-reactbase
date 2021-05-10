aebdea2893c506f0d1c1c96312b43ff8cd0e0ab6
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-10 19:55:39 +0900

    checkpoint - deleted projects.js chain

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "../../App.css";
-import CardProject from "../CardProject";
+
 
 
 function Projects() {
     return (
         <>
             <h1 className="projects"></h1>
-            <CardProject />
+
 
         </>
     );
