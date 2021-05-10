4c584456d5c58d59ae9304ad4982be308e8138b2
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-05-10 20:12:17 +0900

    safe point!

---
+++ src/components/MeCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../App.css";
+import { Button } from "./Button";
+import "./VideoFile.css";
+import Cards from "../components/Cards";
+
+function SkillCard() {
+    return (
+        <div className="skill-container">  
+
+            <Cards />
+
+        </div>
+
+    )
+}
+
+export default SkillCard
